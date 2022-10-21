adf5d93cbe2b745d846ea40eb18e4969855b6676
TheDokTor <orkv1234@gmail.com>
2022-10-21 10:03:40 +0300

    Log files

---
+++ SortingGUI/Logs/Config.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SortingGUI.Logs
+{
+    public class Config
+    {
+        private string path;
+        public Config(string path)
+        {
+            this.path = path;
+        }
+
+        public string Path { get { return path; } }
+
+        public void InitializeFile() //intlz the config file
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("[Options]");
+            sb.AppendLine("Show Array=false");
+            sb.AppendLine("Show Progress=true");
+            sb.AppendLine("Show Date=true");
+
+            var f = File.CreateText(path);
+            f.Write(sb.ToString());
+            f.Close();
+        }
+
+
+        public Dictionary<string, string> GetConfig()
+        {
+            var lines = File.ReadAllLines(path);
+            var dict = new Dictionary<string, string>();
+
+            foreach (var s in lines)
+            {
+                try
+                {
+                    var split = s.Split('=');
+                    dict.Add(split[0], split[1]);
+                }
+                catch { }
+            }
+
+            return dict;
+        }
+
+
+        public bool IsInitialized()
+        {
+            if(File.Exists(path))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SortingGUI/Logs/SaveData.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SortingGUI.Logs
+{
+    public class SaveData
+    {
+        int[] arr;
+        double[] progress;
+        int loops;
+        double time;
+        string sortingType;
+        public Config configFile;
+        Dictionary<string, string> config;
+
+        public SaveData(int[] arr, int loops, double time, double[] progress, string sortingType)
+        {
+            this.arr = arr;
+            this.loops = loops;
+            this.time = time;
+            this.progress = progress;
+            this.sortingType = sortingType;
+
+            //string configPath = Environment.SpecialFolder.MyDocuments + "/SortingGUI/config.ini";
+            string configPath = "./config.ini";
+            configFile = new Config(configPath);
+
+            if (!File.Exists(configPath))
+            {
+                configFile.InitializeFile();
+            }
+            config = configFile.GetConfig();
+        }
+
+        public void toText(string path)
+        {
+            string txt = "";
+            txt += sortingType + " (" + DateTime.Now + "):" + "\n";
+
+
+            if (config["Show Date"].ToLower() == "true")
+                txt += "Length: " + arr.Length +  " Loops: " + loops + " | Time: " + time + " | Sorting type: " + sortingType + "\n";
+            else
+                txt += "Length: " + arr.Length + " Loops: " + loops + " | Sorting type: " + sortingType + "\n";
+
+            if (config["Show Array"].ToLower() == "true")
+            {
+                txt += "sorted array: [ ";
+                foreach (int i in arr)
+                {
+                    txt += i + " ";
+                }
+                txt += "]\n";
+            }
+
+
+            
+            txt += "progress: \n + [ ";
+
+            if (config["Show Progress"].ToLower() == "true")
+            {
+                foreach (int i in progress)
+                {
+                    txt += i + " ";
+                }
+                txt += "]\n";
+            }
+            else
+                txt += "DISABLED\n";
+
+
+
+
+            for(int i = 0; i < 100; i++)
+            {
+                txt += "-";
+            }
+            txt += "\n";
+
+            if (!File.Exists(path))
+            {
+                var f = File.Create(path);
+                f.Close();
+            }
+
+            File.AppendAllText(path, txt + Environment.NewLine);
+        }
+    }
+}
